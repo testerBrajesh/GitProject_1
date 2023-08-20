4957497550f376ce7951818e3c83c884cd55e5a5
testerBrajesh <bkewat582@gmail.com>
2023-08-20 17:13:47 +0530

    fake commit

--- src/git/TestGitDemo1.java
+++ src/git/TestGitDemo1.java
@@ -7,11 +7,15 @@ public class TestGitDemo1 {
 	System.out.println("Commit On master 1 ");
 
 	
+<<<<<<< HEAD
 	System.out.println("Create issue Commit On master 1 ");
 	
 	
 	
 	
 
+=======
+	System.out.println("Update issue fixed On Sp_2 On master 1 ");
+>>>>>>> Sprint-2
 	
 }}
